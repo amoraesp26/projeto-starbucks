cbb58204074a4e083eee44c7b4a19514ef2e7030
anderson moraes pereira <amp260991@hotmail.com>
2025-09-18 10:23:55 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let imagem = document.querySelector(".imagem-copo")
+let fundo = document.querySelector(".circulo")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+
+
+function trocaFundo(cor) {
+    fundo.style.background = cor
+}
